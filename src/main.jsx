c43e6457f14af0633a98f4af538376048742d27c
SyedEbad-007 <syedebad192@gmail.com>
2025-10-09 11:11:30 +0500

    FIX: Remove basename for Vercel deployment

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import { AppProvider } from './context/AppContext';
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   
-    <BrowserRouter>
+    <BrowserRouter >
       <AppProvider>
       <App />
       </AppProvider>

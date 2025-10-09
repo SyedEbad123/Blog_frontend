d53cdb0540ac8d9166eb8558e87ea2c4d15d6d75
SyedEbad-007 <syedebad192@gmail.com>
2025-10-09 11:28:41 +0500

    FIX: Set base path in Vite config for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
+  // Add this line to fix the asset paths on Vercel
+  base: './', 
+
   plugins: [
-     tailwindcss(),
-    react()]
+    tailwindcss(),
+    react()
+  ]
 });

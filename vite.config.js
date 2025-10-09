de67b254555a264699a9ca4f5140f04dfdba1ef1
SyedEbad-007 <syedebad192@gmail.com>
2025-10-09 12:11:51 +0500

    FIX: Restore plugins in Vite config

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  // REMOVE the base: './' line. This makes paths absolute (e.g., /assets/index.js)
+  
   plugins: [
-     tailwindcss(),
-     react()
+    tailwindcss(),
+    react()
   ]
-});
+})
